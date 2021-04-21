0176bc2418cd941ef8de69a7878abe25fbc36a5f
Tong Quang Truong <truongwerk@gmail.com>
2021-04-21 19:59:28 +0700

    done player vs player

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
+//Game Board object
 const gameBoard = () => {
 	const board = ["", "", "", "", "", "", "", "", ""];
 	const setBoard = (index, sign) => {
@@ -15,29 +16,95 @@ const gameBoard = () => {
 	return { board, setBoard, freeSpaces };
 };
 
+function checkFreeSpace(freeSpaces, index) {
+	for (let i = 0; i < freeSpaces.length; i++) {
+		if (freeSpaces[i] == index) {
+			return true;
+		}
+	}
+	return false;
+}
+
 const gameControllerPvP = () => {
 	let sign = "X";
+	let result = "continue";
 	const game = gameBoard();
 	const boards = document.querySelectorAll(".square");
 	boards.forEach((board) => {
 		board.addEventListener("click", function () {
 			if (checkFreeSpace(game.freeSpaces(), board.id)) {
-				game.setBoard(board.id, sign);
-				if (sign == "X") {
-					board.innerText = "X";
-					sign = "O";
-				} else {
-					board.innerText = "O";
-					sign = "X";
+				if (result === "continue") {
+					game.setBoard(board.id, sign);
+					if (sign === "X") {
+						sign = "O";
+					} else {
+						sign = "X";
+					}
+					result = gameResultController(game.board, game.freeSpaces());
+					displayController(game, sign, result);
 				}
-				console.log(game.board);
-				console.log(game.freeSpaces().length);
-				console.log(gameResultController(game.board, game.freeSpaces()));
 			}
 		});
 	});
 };
 
+function displayController(game, sign, result) {
+	const imageX = document.createElement("img");
+	imageX.src = "../styles/X_icon.png";
+	imageX.style.width = "75%";
+	const imageO = document.createElement("img");
+	imageO.src = "../styles/O_icon.png";
+	imageO.style.width = "75%";
+	//Display game board
+	for (let i = 0; i < 9; i++) {
+		const box = document.getElementById(`${i}`);
+		if (!box.firstChild) {
+			if (game.board[i] === "X") {
+				box.appendChild(imageX);
+			} else if (game.board[i] === "O") {
+				box.appendChild(imageO);
+			}
+		}
+	}
+	//Display announcer
+	const announcer = document.querySelector("#announcer");
+	if (result == "continue") {
+		if (sign === "X") {
+			announcer.innerText = "This is X's turn";
+		} else {
+			announcer.innerText = "This is 0's turn";
+		}
+	} else if (result === "X") {
+		announcer.innerText = "Player X win";
+	} else if (result === "O") {
+		announcer.innerText = "Player O win";
+	} else {
+		announcer.innerText = "Draw!";
+	}
+	console.log(game.board);
+	console.log(game.freeSpaces().length);
+	console.log(result);
+}
+
+const clearAndCreate = () => {
+	const container = document.querySelector("#container");
+	const reset = () => {
+		while (container.firstChild) {
+			container.removeChild(container.firstChild);
+		}
+	};
+	const create = () => {
+		for (let i = 0; i < 9; i++) {
+			const index = document.createElement("div");
+			index.classList = "square";
+			index.id = i;
+			container.appendChild(index);
+		}
+	};
+	return { reset, create };
+};
+
+//Stop game and decide game result
 function gameResultController(board, freeSpaces) {
 	for (let i = 0; i <= 7; i += 3) {
 		if (board[i] === board[i + 1] && board[i] === board[i + 2]) {
@@ -72,23 +139,17 @@ function gameResultController(board, freeSpaces) {
 	} else if (freeSpaces.length == 0) {
 		return "draw";
 	}
+	return "continue";
 }
-(function () {
-	const container = document.querySelector("#container");
-	for (let i = 0; i < 9; i++) {
-		const index = document.createElement("div");
-		index.classList = "square";
-		index.id = i;
-		container.appendChild(index);
-	}
-	gameControllerPvP();
-})();
 
-function checkFreeSpace(freeSpaces, index) {
-	for (let i = 0; i < freeSpaces.length; i++) {
-		if (freeSpaces[i] == index) {
-			return true;
-		}
-	}
-	return false;
-}
+const resetBtn = document.querySelector("#resetButton");
+resetBtn.addEventListener("click", function () {
+	const board = clearAndCreate();
+	board.reset();
+	board.create();
+	gameControllerPvP();
+});
+
+const board = clearAndCreate();
+board.create();
+gameControllerPvP();
